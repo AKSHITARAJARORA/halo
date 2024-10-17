de7442a5c410bb9d2d94da7276576326b0c3d3dc
AKSHITARAJARORA <85892300+AKSHITARAJARORA@users.noreply.github.com>
2024-10-17 22:22:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var student={
+    name: 'Akshita',
+    age: '13',
+    grade: '8',
+    hobbies: 'Drawing',
+    fav_sport: 'Basketball'};
+var color=["Black", "White", "Purple", "Pink", "Blue", "Orange"]
+console.log("Student name is", student.name,
+"her fav sport is", student.fav_sport)
